edb35d69ea91985af0dfd989a6eeb526f7e6be2c
tax1t <h.hsyn66@hotmail.com>
2026-09-07 22:11:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import sys
+import os
 import os
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
