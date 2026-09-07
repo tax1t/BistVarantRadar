c0850560e0760dcbbdca33fe4c10110a6836ad4f
tax1t <h.hsyn66@hotmail.com>
2026-09-07 22:33:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import json
 import pandas as pd
 
-from data.pipeline import DataPipeline
+from pipeline import DataPipeline
 
 from analysis.technical import TechnicalEngine
 from analysis.fundamental import FundamentalEngine
