4c76970fcbd7c1dc7b9f8a40655f8ce93793725b
tax1t <h.hsyn66@hotmail.com>
2026-09-08 12:55:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -539,7 +539,8 @@ if __name__ == "__main__":
 
     if __name__ == "__main__":
         try:
-            app.run(host="0.0.0.0", port=port)
+           import os
+port = int(os.environ.get("PORT", 10000))
         except Exception as e:
             import traceback
             traceback.print_exc()
